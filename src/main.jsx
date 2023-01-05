d524c7b54a7777ebb35254fa8b258e88c29755fe
Christian R <christianriis.kodehode@gmail.com>
2023-01-05 11:32:43 +0100

    removed-app-stuff

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+// LIBRARIES
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+// COMPONENTS
+import App from "./App";
+
+// CSS
+import "./styles/basic.css";
+import "./styles/global.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
